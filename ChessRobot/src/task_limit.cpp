aae71095062996f64dc952f2172ff3a82fba9cae
cagena <cagena65@gmail.com>
2023-05-25 12:52:21 -0700

    Update task_limit.cpp

--- ChessRobot/src/task_limit.cpp
+++ ChessRobot/src/task_limit.cpp
@@ -20,18 +20,10 @@ void task_limit (void* p_params)
         if (limitswitch_x.isPressed())
         {
             x_done.put(1);
-        }
-        if (limitswitch_x.isReleased())
-        {
-            x_done.put(0);
-        }        
+        }     
         if (limitswitch_y.isPressed())
         {
             y_done.put(1);
-        }
-        if (limitswitch_y.isReleased())
-        {
-            y_done.put(0);
         }
         vTaskDelay(10);
     }
